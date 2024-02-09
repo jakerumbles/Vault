d18182808d173d99bdd373dec430d1886d5d34d5
Jake Edwards <edwardsjake@live.com>
2024-02-09 17:03:37 -0600

    Deposit SOL is working!

--- programs/vault/src/lib.rs
+++ programs/vault/src/lib.rs
@@ -1,4 +1,4 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*, system_program};
 
 declare_id!("7JCk8GRuxk8KfE6ttP7qx3QdGPDCKKvHyQHuJmHZCAn");
 
@@ -6,10 +6,80 @@ declare_id!("7JCk8GRuxk8KfE6ttP7qx3QdGPDCKKvHyQHuJmHZCAn");
 pub mod vault {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    /// Initializes a new vault and sets the vault configuration.
+    /// `max_balance` is expected to be in lamports
+    pub fn initialize(ctx: Context<Initialize>, max_balance: u64) -> Result<()> {
+        let vault_info = &mut ctx.accounts.vault_info;
+        vault_info.max_balance = max_balance;
+        vault_info.bump = ctx.bumps.vault_info;
+        msg!(
+            "Vault initialized. Max balance: {} SOL, bump: {}",
+            max_balance,
+            vault_info.bump
+        );
+        Ok(())
+    }
+
+    /// Deposits SOL into the vault
+    pub fn deposit_sol(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        let vault_info = &ctx.accounts.vault_info;
+        let new_balance = vault_info.get_lamports() + amount;
+        if new_balance > vault_info.max_balance {
+            return Err(ErrorCode::DepositAmountTooLarge.into());
+        }
+        msg!("Depositing {} lamports into the vault...", amount);
+
+        // Transfer the SOL from the depositor to the vault
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.depositor.to_account_info(),
+                to: ctx.accounts.vault_info.to_account_info(),
+            },
+        );
+        system_program::transfer(cpi_context, amount)?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    #[account(
+        init,
+        payer = provider,
+        space = 8 + VaultInfo::LEN,
+        seeds = [b"SOLvault"],
+        bump
+    )]
+    pub vault_info: Account<'info, VaultInfo>,
+    #[account(mut)]
+    pub provider: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct Deposit<'info> {
+    #[account(mut)]
+    pub vault_info: Account<'info, VaultInfo>,
+    #[account(mut)]
+    pub depositor: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct VaultInfo {
+    // pub accepted_token: Pubkey,
+    pub max_balance: u64,
+    pub bump: u8,
+}
+
+impl VaultInfo {
+    pub const LEN: usize = 8 + 1;
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Deposit amount too large. Would cause vault to exceed max balance.")]
+    DepositAmountTooLarge,
+}
--- tests/vault.ts
+++ tests/vault.ts
@@ -1,16 +1,52 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Vault } from "../target/types/vault";
+import { BN } from "bn.js";
+import { PublicKey, LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 describe("vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
 
   const program = anchor.workspace.Vault as Program<Vault>;
 
+  const [vaultInfoPDA, _] = PublicKey.findProgramAddressSync(
+    [anchor.utils.bytes.utf8.encode("SOLvault")],
+    program.programId
+  );
+
   it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const maxBalance = new BN(10 * LAMPORTS_PER_SOL);
+
+    const tx = await program.methods
+      .initialize(maxBalance)
+      .accounts({
+        vaultInfo: vaultInfoPDA,
+        provider: provider.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log(
+      `View transaction: https://explorer.solana.com/tx/${tx}?cluster=custom`
+    );
+  });
+
+  it("Deposits 1.5 SOL", async () => {
+    const amount = new BN(1.5 * LAMPORTS_PER_SOL);
+
+    const tx = await program.methods
+      .depositSol(amount)
+      .accounts({
+        vaultInfo: vaultInfoPDA,
+        depositor: provider.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
+
+    console.log(
+      `View transaction: https://explorer.solana.com/tx/${tx}?cluster=custom`
+    );
   });
 });
