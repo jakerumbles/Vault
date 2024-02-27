e52b076d2179d7b607631f97e3744c7c17cac8e2
Jake Edwards <edwardsjake@live.com>
2024-02-27 17:00:00 -0600

    Update Deposit.tsx

--- app/components/Deposit.tsx
+++ app/components/Deposit.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "@nextui-org/button";
 import { Input } from "@nextui-org/react";
-import { DepositData } from "../models/DepositData";
+// import { DepositData } from "../models/DepositData";
 import { Address, web3 } from "@coral-xyz/anchor";
 import * as anchor from "@coral-xyz/anchor";
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
